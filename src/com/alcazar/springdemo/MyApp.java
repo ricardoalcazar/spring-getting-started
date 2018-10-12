d5a7767e4b5236b67e6ca5cfe3313d7788b3ccc5
Ricardo Alcazar <ricardo.d.alcazar@gmail.com>
2018-10-12 17:17:53 -0500

    Added SLF4j logging. Created simple demo classes.

--- src/com/alcazar/springdemo/MyApp.java
+++ src/com/alcazar/springdemo/MyApp.java
@@ -1,20 +1,33 @@
 package com.alcazar.springdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class MyApp {
+
+    private static Logger logger = LoggerFactory.getLogger(MyApp.class);
+
 
     /**
      * Main application
-     * @param args
+     * @param args default arguments
      */
     public static void main(String[] args){
 
-        // create the object
+        logger.info("Running MyApp.main()");
 
+        // create the object
+        BaseballCoach theCoach = new BaseballCoach();
 
         // use the object
+        logger.info(theCoach.getDailyWorkout());
+
+
+        logger.info("MyApp.main() finished.");
 
     }
 
 
-    
+
 }
